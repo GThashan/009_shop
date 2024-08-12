52cd1b0948ceaa743dc9b61026b2ec2cfe12878f
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-12 17:20:06 +0530

    create backend environment

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,26 @@
-console.log("working");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+export const conn = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database connection successful");
+        
+    } catch (error) {
+        console.log("Error connecting to database:", error);
+    }
+};
+
+conn();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});

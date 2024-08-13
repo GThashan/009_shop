907febeff79634380fddf2947f55735a78e4438a
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-13 15:26:31 +0530

    sub menu

--- Frontend/009/src/App.jsx
+++ Frontend/009/src/App.jsx
@@ -9,6 +9,7 @@ import About from './Pages/About'
 import OwnerLogin from './Admin/OwnerLogin'
 import Dashboard from './Admin/Dashboard'
 import AdMenu from './Admin/AdMenu'
+import SubMenu from './Admin/SubMenu'
 function App() {
 
   
@@ -26,6 +27,7 @@ function App() {
       <Route path='/admin' element={<OwnerLogin/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path='/admenu' element={<AdMenu/>}/>
+      <Route path='/submenu' element={<SubMenu/>}/>
      </Routes>
      </BrowserRouter>
     </>
